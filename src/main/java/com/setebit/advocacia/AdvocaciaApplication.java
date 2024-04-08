e1b5b93e461654d58b4bfb98f701b77cf5b679cb
Claudemir <claudemirramosferreira@gmail.com>
2024-04-08 08:01:59 -0300

    ajuste para deploy no tomcat

--- src/main/java/com/setebit/advocacia/AdvocaciaApplication.java
+++ src/main/java/com/setebit/advocacia/AdvocaciaApplication.java
@@ -2,9 +2,10 @@ package com.setebit.advocacia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdvocaciaApplication {
+public class AdvocaciaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvocaciaApplication.class, args);
